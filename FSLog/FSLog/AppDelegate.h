c7739602634a643eed5c5a33203759d87b42b963
Eric <pengbobin@qq.com>
2018-12-19 16:51:24 +0800

    创建FSLog

---
+++ FSLog/FSLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSLog
+//
+//  Created by ericbbpeng(彭博斌) on 2018/12/19.
+//  Copyright © 2018 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FSLog/FSLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FSLog
+//
+//  Created by ericbbpeng(彭博斌) on 2018/12/19.
+//  Copyright © 2018 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
