5af31b0a6a3d4f6f71685dd3cb4506931809d160
Eric <pengbobin@qq.com>
2019-01-09 16:20:47 +0800

    develop

--- python_script/photo.py
+++ python_script/photo.py
@@ -1,22 +1,29 @@
 import os
 import time
+import shutil
 
 # '''把时间戳转化为时间: 1479264792 to 2016-11-16 10:53:12'''
 def TimeStampToTime(timestamp):
     timeStruct = time.localtime(timestamp)
-    return time.strftime('%Y-%m-%d %H:%M:%S',timeStruct)
+    return time.strftime('%Y年%m月',timeStruct)
 
 # '''获取文件的创建时间'''
 def get_FileCreateTime(filePath):
-    t = os.path.getctime(filePath)
+    t = os.path.getmtime(filePath)
     return TimeStampToTime(t)
 
 
 #遍历根目录
-for root,dirs,files in os.walk('/Users/eric/Desktop/'):
+for root,dirs,files in os.walk('/Users/ericbbpeng/Pictures/照片测试/原始'):
     for file in files:
         file_name = os.path.join(root,file)
-        t = get_FileCreateTime(file_name)
-        print(file_name, t)
+        photoTime = get_FileCreateTime(file_name)
+        newDir = os.path.join('/Users/ericbbpeng/Pictures/照片测试/new/', photoTime)
+        if not os.path.exists(newDir):
+            os.makedirs(newDir)
+        # print(file_name, t)
+        newFilePath = os.path.join(newDir, file)
+        shutil.move(file_name, newFilePath)
 
-os.makedirs("/Users/eric/Desktop/hellowow")
+# os.makedirs("/Users/ericbbpeng/Desktop/hellowow")
+os.mkdir()
